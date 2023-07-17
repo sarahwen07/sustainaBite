c686f001ffd33efa4e714ebcc830c0d5dec28d64
Sarah Wen <sawen1207@gmail.com>
2023-07-17 15:01:07 -0700

    Initial Commit

---
+++ sustainaBite/sustainaBiteApp.swift
@@ -0,0 +1,17 @@
+//
+//  sustainaBiteApp.swift
+//  sustainaBite
+//
+//  Created by scholar on 7/17/23.
+//
+
+import SwiftUI
+
+@main
+struct sustainaBiteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
