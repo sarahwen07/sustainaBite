513ef42cb3843fef2f2c273cce3dd71f5630711b
Sarah Wen <sawen1207@gmail.com>
2023-07-18 09:02:08 -0700

    test

--- sustainaBite/WelcomeView.swift
+++ sustainaBite/WelcomeView.swift
@@ -15,6 +15,7 @@ struct WelcomeView: View {
     var body: some View {
         NavigationStack {
             ZStack {
+                Text("hello")
                 Color("almond")
                     .ignoresSafeArea()
                 VStack {
